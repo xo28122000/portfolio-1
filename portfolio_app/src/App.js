30277b88cea125b2092acc4be24a559a2769b6f1
xo28122000 <jainam.2000@yahoo.com>
2019-11-18 08:21:32 -0800

    init

---
+++ portfolio_app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Earth } from "./assets/Earth.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <div className="helloworld">Coffee></div>
+        </li>
+        <li>Tea</li>
+        <li>Milk</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
