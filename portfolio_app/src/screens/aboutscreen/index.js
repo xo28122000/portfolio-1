e01d75aef4d6fe666a697964b21ef91ab0729e0b
xo28122000 <jainam.2000@yahoo.com>
2020-02-07 17:49:40 -0800

    instructions added to about page
    blinking instructions added at the bottom of the page

--- portfolio_app/src/screens/aboutscreen/index.js
+++ portfolio_app/src/screens/aboutscreen/index.js
@@ -9,7 +9,11 @@ import experiencecontent from "./experiencecontent";
 import personallifecontent from "./personallifecontent";
 
 class AboutScreen extends Component {
-  state = { currentContent: Homecontent, actionListeneradded: false };
+  state = {
+    currentContent: Homecontent,
+    actionListeneradded: false,
+    blinkText: "Use Arrow keys to navigate, enter to view and esc to go back"
+  };
   render() {
     const findSelectedEle = eleList => {
       var i = 0;
@@ -49,25 +53,38 @@ class AboutScreen extends Component {
     const content1 = (
       <div>>>>>>>>>>>>>>>>>>>>-------- ERROR -------->>>>>>>>>>>>>>>>>>></div>
     );
+    const navigatorText =
+      "Use Arrow keys to navigate, enter to view and esc to go back";
+    const escapeText = "Use esc to go back";
+
     document.onkeyup = e => {
       if (this.state.currentContent === Homecontent) {
         var i = arrowNavigation(e);
         if (e.keyCode === 13) {
           if (i === 0) {
-            this.setState({ currentContent: academiccontent });
+            this.setState({
+              currentContent: academiccontent,
+              blinkText: escapeText
+            });
           } else if (i === 1) {
-            this.setState({ currentContent: experiencecontent });
+            this.setState({
+              currentContent: experiencecontent,
+              blinkText: navigatorText
+            });
           } else if (i === 2) {
             window.open(
               "https://xo28122000.github.io/portfolio/files/Shah_Jainam.pdf",
               "_blank"
             );
           } else if (i === 3) {
-            this.setState({ currentContent: personallifecontent });
+            this.setState({
+              currentContent: personallifecontent,
+              blinkText: escapeText
+            });
           } else if (i === 4) {
             this.setState({ currentContent: <Redirect to="/project" /> });
           } else {
-            this.setState({ currentContent: content1 });
+            this.setState({ currentContent: content1, blinkText: escapeText });
           }
         } else if (e.key === "Escape") {
           this.setState({ currentContent: <Redirect to="/" /> });
@@ -83,7 +100,10 @@ class AboutScreen extends Component {
           }
         } else if (e.key === "Escape") {
           if (this.state.currentContent !== Homecontent) {
-            this.setState({ currentContent: Homecontent });
+            this.setState({
+              currentContent: Homecontent,
+              blinkText: navigatorText
+            });
           } else {
             console.log("use escape to go back");
           }
@@ -106,7 +126,7 @@ class AboutScreen extends Component {
           <div
             style={{
               paddingTop: "25vh",
-              paddingBottom: "20vh"
+              paddingBottom: "15vh"
             }}
           >
             <div
@@ -122,6 +142,21 @@ class AboutScreen extends Component {
               <div id="displayContent">{this.state.currentContent}</div>
             </div>
           </div>
+        </div>
+        <div
+          id="instruction"
+          className="PressStart blinking"
+          style={{
+            position: "fixed",
+            width: "100%",
+
+            bottom: "1vh",
+            color: "#000",
+            height: "5vh",
+            textAlign: "center"
+          }}
+        >
+          {this.state.blinkText}
         </div>
       </div>
     );
