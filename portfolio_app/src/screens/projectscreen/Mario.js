34e218bb40df37d6af3ea5093e7abdd353ca8eb8
xo28122000 <jainam.2000@yahoo.com>
2020-03-13 21:29:09 -0700

    pow pow pow

--- portfolio_app/src/screens/projectscreen/Mario.js
+++ portfolio_app/src/screens/projectscreen/Mario.js
@@ -3,14 +3,14 @@ import React, { Component } from "react";
 import "./Mario.css";
 
 class Mario extends Component {
-  state = { playerLeftPosition: 500, playerBottomPosition: 20 };
+  state = { playerLeftPosition: 50, playerBottomPosition: 20 };
   render() {
-    const movementSpeed = 30;
+    const movementSpeed = 15;
     const runchars = ["./marioRun/marioRun2", "./marioRun/marioRun3"];
     document.onkeydown = e => {
       const marioPlayer = document.getElementById("marioPlayer");
 
-      console.log(e.code === "ArrowLeft" && e.code === "ArrowUp");
+      // console.log(e.code === "ArrowLeft" && e.code === "ArrowUp");
       if (e.code === "ArrowLeft") {
         if (this.state.playerLeftPosition - movementSpeed >= 0) {
           marioPlayer.style.transform = "";
@@ -21,7 +21,7 @@ class Mario extends Component {
         }
       } else if (e.code === "ArrowRight") {
         if (
-          this.state.playerLeftPosition + 25 <=
+          this.state.playerLeftPosition + 85 <=
           marioPlayer.offsetParent.offsetWidth
         ) {
           marioPlayer.style.transform = "scaleX(-1)";
@@ -30,6 +30,17 @@ class Mario extends Component {
           });
         }
       } else if (e.code === "ArrowUp") {
+        var middlePos = marioPlayer.offsetParent.offsetWidth / 2;
+
+        if (
+          this.state.playerLeftPosition >= middlePos - 35 &&
+          this.state.playerLeftPosition <= middlePos + 35
+        ) {
+          console.log("came here");
+          this.props.changePrompt();
+        } else {
+          console.log("came here instead");
+        }
         this.setState({
           playerBottomPosition: 50
         });
@@ -40,6 +51,7 @@ class Mario extends Component {
         }, 300);
       }
     };
+    // this.props.changePrompt();
     return (
       <div id="marioRoot" className="container-fluid" style={{}}>
         <div id="blockRegion"></div>
--- portfolio_app/src/screens/projectscreen/index.js
+++ portfolio_app/src/screens/projectscreen/index.js
@@ -3,12 +3,73 @@ import Mario from "./Mario";
 import "./project.css";
 class ProjectScreen extends Component {
   state = {};
+  projects = [
+    <p>
+      Name: HealthDe.tech - Personal Health Detector - link:
+      github.com/xo28122000/HealthDetect
+      <br />◦ Developed a React.js front-end for users to sign up/in and upload
+      a picture of the diseased body part.
+      <br />◦ Trained CNN models to detect diseases using Tensorflow API and
+      Google Cloud Platform.
+      <br />◦ Deployed a restful flask server to use the trained models to
+      predict the disease in pictures requested by the user.
+      <br />
+    </p>,
+    `Name: Recovery Coach — MIT Health 2019
+      <br /> title: Personalized physical rehab coach and research platform
+      <br /> link: github.com/joshuaaguilar20/MIT-HealthHacks
+      <br /> contribution:
+      <br /> ◦ Developed a web app to increase the efficiency of physical
+      therapy centers by giving the power of supervising to AI.
+      <br /> ◦ Traced the movements of joints using poseNet and compared to the
+      correct exercise pattern uploaded by the PT.
+      <br />
+      ◦ Won 3rd place in self care in chronic diseases track.
+      <br />
+   `,
+    `Name: RideAid — HackMobility 2019\n
+    title: Carpooling made simpler, safer and efficient\n
+    link: github.com/helenamerk/hackmobility\n
+    contribution:\n
+  ◦ Developed a React Native app for user to start/join a pool and integrated the SmartCar’s API to verify and unlock the car.\n◦ Developed a data processing back-end to clean and serve the data to front-end after fetching it from a PostgreSQL database.\n◦ Won the Shell Grand Prize and an award for the best use of Smartcar API.
+  `
+  ];
   render() {
+    const changePrompt = () => {
+      console.log("changed the prompt");
+    };
     return (
       <div id="projectRoot">
-        <div id="instructionArea"></div>
+        <div id="instructionArea">
+          <div
+            id="instruction"
+            className="PressStart blinking"
+            style={{
+              width: "100%",
+              paddingTop: "20px",
+              color: "#000",
+              height: "5vh",
+              textAlign: "center"
+            }}
+          >
+            Use arrows to move and jump. hit POW.
+          </div>
+          <div
+            className="PressStart"
+            style={{
+              width: "100%",
+              paddingTop: "20px",
+              fontSize: "10px"
+              // color: "#000",
+              // height: "5vh",
+              // textAlign: "center"
+            }}
+          >
+            {this.projects[0]}
+          </div>
+        </div>
         <div id="gameArea">
-          <Mario />
+          <Mario changePrompt={changePrompt} />
         </div>
       </div>
     );
